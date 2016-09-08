59182340935dd07fd3867048f0dbcde142ddf68b
Varun Dey <varun.silversurfer@gmail.com>
2016-09-08 14:53:34 +0530

    added README

---
+++ README.md
@@ -0,0 +1,9 @@
+# life-goals
+
+```sh
+$ source venv/bin/activate
+(venv) $ pip install -r requirements.txt
+(venv) $ python manage.py migrate
+(venv) $ python manage.py makemigrations
+(venv) $ python manage.py runserver
+```
--- app/templatetags/app_extras.py
+++ app/templatetags/app_extras.py
@@ -1,8 +1,7 @@
 from django import template
-# from todo.app.models import Event
 
 register = template.Library()
-# events = Event.objects.all().filter(done=True)
+
 @register.filter
 def count_done(value):
     return (100*value.filter(done=True).count()/value.count())
