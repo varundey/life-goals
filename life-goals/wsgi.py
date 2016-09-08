1fde4244ed064f7b08c49097a0978c1a4b768a85
Varun Dey <varun.silversurfer@gmail.com>
2016-09-08 14:59:11 +0530

    changed project name

--- life-goals/wsgi.py
+++ life-goals/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for todo project.
+WSGI config for life-goals project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "life-goals.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
